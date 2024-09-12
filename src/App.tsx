c25213c1af2ab2abde62aacba861b002cb0bc448
william5468 <123747343+william5468@users.noreply.github.com>
2024-09-12 15:33:05 +0800

    Update App.tsx _ 13 test category details

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,15 @@ const App: React.FC = () => {
     { id: 1, name: "Nutrition" },
     { id: 2, name: "Prognosis" },
     { id: 3, name: "Infection" },
+    { id: 4, name: "Sepsis" },
+    { id: 5, name: "Kidney" },
+    { id: 6, name: "Liver" },
+    { id: 7, name: "Biliary" },
+    { id: 8, name: "Cell damage" },
+    { id: 9, name: "Anemia" },
+    { id: 10, name: "Coagulation" },
+    { id: 11, name: "Electrolyte" },
+    { id: 12, name: "Blood gas" },
     // Add more tests as needed
   ];
 
@@ -55,6 +64,57 @@ const App: React.FC = () => {
       { id: 16, name: "IG" },
       { id: 4, name: "CRP" },
     ],
+    4: [
+      { id: 13, name: "PLT" },
+      { id: 17, name: "FIB" },
+      { id: 4, name: "CRP" },
+    ],    
+    5: [
+      { id: 6, name: "CRE" },
+      { id: 18, name: "URE" },
+      { id: 19, name: "CA" },
+      { id: 20, name: "P" },
+    ],     
+    6: [
+      { id: 21, name: "ALT" },
+      { id: 22, name: "AST" },
+      { id: 23, name: "GGT" },
+      { id: 9, name: "TB" },
+      { id: 1, name: "ALB" },
+    ],  
+    7: [
+      { id: 23, name: "GGT" },
+      { id: 24, name: "ALP" },
+    ],     
+    8: [
+      { id: 25, name: "LDH" },
+      { id: 10, name: "CK" },
+      { id: 26, name: "HsTNI" },
+      { id: 22, name: "AST" },
+    ],    
+    9: [
+      { id: 7, name: "HB" },
+      { id: 27, name: "MCV" },
+      { id: 12, name: "URE/CRE" },
+    ],    
+    10: [
+      { id: 28, name: "PT" },
+      { id: 29, name: "APTT" },
+      { id: 17, name: "FIB" },
+      { id: 30, name: "D-dimer" },
+    ],    
+    11: [
+      { id: 31, name: "NA" },
+      { id: 32, name: "K" },
+      { id: 33, name: "CL" },
+    ],         
+    12: [
+      { id: 34, name: "PH" },
+      { id: 35, name: "PCO2" },
+      { id: 36, name: "HCO3" },
+      { id: 37, name: "PO2" },      
+      { id: 38, name: "SaO2" },      
+    ],         
   };
 
   const [selectedTestId, setSelectedTestId] = useState<number>(1); // Default to first test
